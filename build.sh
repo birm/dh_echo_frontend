3067bcd5efa0a8cbc291ec1f7f12f115be048413
Birm <birm@rbirm.us>
2018-02-01 02:37:03 -0500

    basic echo frontend

---
+++ build.sh
@@ -0,0 +1,11 @@
+API_URL = $1
+# install dependencies
+apt-get --yes --force-yes install npm
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+npm install
+
+# set api url
+sed -i "s/original/$API_URL/g" index.html
+# run
+nodejs index.js &&
